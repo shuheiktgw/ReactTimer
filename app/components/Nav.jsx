c7acebf427b73e5b15d11e0d26e1a8f23aedeca6
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-10-27 17:27:49 +0900

    initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+    return(
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">
+              React Timer
+            </li>
+            <li>
+              <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by <a href="https://www.github.com" target="_black">Shuhei Kitagawa</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+};
+
+module.exports = Nav;
