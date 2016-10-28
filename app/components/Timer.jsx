1a92705ff08fd4561a3515b1ad4a560fd1dececd
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-10-28 09:18:05 +0900

    add navigation

---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function(props){
+    return(
+      <div>
+        <h1>Timer</h1>
+      </div>
+      )
+  }
+});
+
+module.exports= Timer;
